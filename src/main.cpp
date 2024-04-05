e767292cd35116b5d05dde00e3adfa132c62b1e0
Fankeks <fkoleda.2012@gmail.com>
2024-04-05 12:34:34 +0300

    USART0 rx, tx interrupt library

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,39 @@
 #include <Arduino.h>
-
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <My_Serial.h>
 
 #define LEDPIN PIN_PB7
 
+unsigned char ch;
 
-void setup()
+ 
+void setup(void)
 {
-  // Частота МК 16 МГц
   pinMode(LEDPIN, OUTPUT);
 
-  cli();
-  UCSR0B = (1 << RXEN0) | (1 << TXEN0);   // Turn on the transmission and reception circuitry
-  UCSR0C = (1 << UCSZ00) | (1 << UCSZ01); // 8 бит сообщение
-  UCSR0C &= ~(1 << USBS0);                // 1 стоповый бит
-  // Скорость
-  UBRR0H = (9600 >> 8);                   // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
-  UBRR0L = 9600;                          // Load lower 8-bits of the baud rate value into the low byte of the UBRR register
-
-  UCSR0B |= (1 << RXCIE0);                // Enable the USART Receive Complete interrupt (USART_RX)
-  sei();
+  My_Serial0::init(9600);
+  My_Serial0::en_interrupt_rx();
+  My_Serial0::en_interrupt_tx();
 }
 
 
 void loop()
 {
-  
+
 }
 
 
 ISR(USART0_RX_vect)
 {
-  char ch = UDR0;           // Считать данные
+  My_Serial0::dis_interrupt_rx();
+  ch = My_Serial0::read_byte();
+  My_Serial0::write_byte(ch);
+  My_Serial0::en_interrupt_rx();
+}
+
+
+ISR(USART0_TX_vect)
+{
   digitalWrite(LEDPIN, !digitalRead(LEDPIN));
 }
