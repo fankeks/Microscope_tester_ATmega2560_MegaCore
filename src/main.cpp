571bf64e637d1db6d4039b37ef2862b99c3f9b23
Fankeks <fkoleda.2012@gmail.com>
2024-04-05 13:27:00 +0300

    Echo bot

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #define LEDPIN PIN_PB7
 
 unsigned char ch;
+unsigned char mes_buffer[64];
+unsigned char ans_buffer[64];
+int count = 0;
+bool flag = false;
 
  
 void setup(void)
@@ -14,7 +18,7 @@ void setup(void)
 
   My_Serial0::init(9600);
   My_Serial0::en_interrupt_rx();
-  My_Serial0::en_interrupt_tx();
+  My_Serial0::dis_interrupt_tx();
 }
 
 
@@ -27,13 +31,27 @@ void loop()
 ISR(USART0_RX_vect)
 {
   My_Serial0::dis_interrupt_rx();
+
   ch = My_Serial0::read_byte();
-  My_Serial0::write_byte(ch);
+  ans_buffer[count++] = ch;
+  if (ch == '\n')
+  {
+    count = 0;
+    My_Serial0::en_interrupt_tx();
+    My_Serial0::write_byte(ans_buffer[count++]);
+  }
+
   My_Serial0::en_interrupt_rx();
 }
 
 
 ISR(USART0_TX_vect)
 {
-  digitalWrite(LEDPIN, !digitalRead(LEDPIN));
+  if (count < 64) My_Serial0::write_byte(ans_buffer[count++]);
+  else 
+  {
+    count = 0;
+    My_Serial0::dis_interrupt_tx();
+    digitalWrite(LEDPIN, !digitalRead(LEDPIN));
+  }
 }
